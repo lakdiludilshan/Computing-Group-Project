7d9cf875d70e21cd5051fb0c4afaee792a06a6c5
lakdiludilshan <lakdiludilshan@gmail.com>
2024-03-12 00:52:30 +0530

    sidebar modified

--- mern-client/src/dashboard/DashboardLayout.jsx
+++ mern-client/src/dashboard/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SideBar from './SideBar'
+import SideBar from "../dashboard/SideBar";
 import { Outlet } from 'react-router-dom'
 
 const DashboardLayout = () => {
--- mern-client/src/dashboard/SideBar.jsx
+++ mern-client/src/dashboard/SideBar.jsx
@@ -1,73 +1,101 @@
-import React, { useState, isOpen } from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/collapse'
 import '../css/sidebar.css'
-import { FaUserAlt, 
-    FaRegChartBar, 
-    FaCommentAlt, 
-    FaBars, 
-    FaTh, 
-    FaShoppingBag, 
-    FaThList } from 'react-icons/fa'
-import { NavLink } from 'react-router-dom'
 
-
-const SideBar = ({Children}) => {
-    const[isopen, setIsOpen] = useState(false);
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    }
-    const menuItems = [
-        {
-            name: 'Dashboard',
-            path: '/admin/dashboard',
-            icon: <FaTh/>,
-        },
-        {   
-            name: 'Dashboard',
-            path: '/admin/dashboard/upload',
-            icon: <FaUserAlt/>,
-        },
-        {
-            name: 'Upload Book',
-            path: '/admin/dashboard/manage',
-            icon: <FaRegChartBar/>,
-        },
-        {
-            name: 'Manage Books',
-            path: '/admin/dashboard/edit',
-            icon: <FaCommentAlt/>,
-        },
-        {
-            name: 'Manage Books',
-            path: '/admin/dashboard/site',
-            icon: <FaShoppingBag/>,
-        },
-        {
-            name: 'Manage Books',
-            path: '/admin/dashboard/wish',
-            icon: <FaThList/>,
-        },
-    ]
-  return (
-    <div className='container'>
-        <div style={{width: isOpen ? "250px" : "50px"}} className='sidebar'>
-            <div className='top_section'>
-                <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Logo</h1>
-                <div style={{marginLeft: isOpen ? "50px" : "0px"}} className='bars'>
-                    <FaBars onClick={toggle}/>
+function SideBar() {
+    return (
+        <div className='Container-fluid'>
+            <div className='row'>
+                <div className='col-auto col-sm-2 bg-dark d-flex flex-column justify-content-between min-vh-100' style={{ width: '210px' }}>
+                    <div className='mt-2'>
+                        <a className='text-decoration-none ms-4 d-flex align-items-center text-white d-none d-sm-inline' role='button'>
+                            <span className='f5-4'>Admin Dashboard</span>
+                        </a>
+                        <hr className='text-white d-none d-sm-block'></hr>
+                        <ul class="nav nav-pills flex-column mt-2 mt-sm-0" id='parentM'>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#" class="nav-link text-white text-center text-sm-start" aria-current="page">
+                                    <i className='bi bi-speedometer2'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Dashboard</span>
+                                </a>
+                            </li>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#" class="nav-link text-white text-center text-sm-start" aria-current="page">
+                                    <i className='bi bi-book'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Upload Book</span>
+                                </a>
+                            </li>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#" class="nav-link text-white text-center text-sm-start" aria-current="page">
+                                    <i className='bi bi-files'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Manage Books</span>
+                                </a>
+                            </li>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#" class="nav-link text-white text-center text-sm-start" aria-current="page">
+                                    <i className='bi bi-people'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Users</span>
+                                </a>
+                            </li>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#submenu" class="nav-link text-white text-center text-sm-start" data-bs-toggle="collapse" aria-current="page">
+                                    <i className='bi bi-grid'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Products</span>
+                                    <i className='bi bi-arrow-down-short ms-2'></i>
+                                </a>
+                                <ul class="nav collapse ms-2 flex-column" id='submenu' data-bs-parent="#parentM">
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white" href="#" aria-current="page">
+                                            <span className='d-none d-sm-inline'>Item </span>1
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link text-white" href="#">
+                                        <span className='d-none d-sm-inline'>Item </span>2
+                                        </a>
+                                    </li>
+                                </ul>
+                                
+                            </li>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#" class="nav-link text-white text-center text-sm-start" aria-current="page">
+                                    <i className='bi bi-arrow-right'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Sign in</span>
+                                </a>
+                            </li>
+                            <li class="nav-item my-1 py-2 py-sm-0">
+                                <a href="#" class="nav-link text-white text-center text-sm-start" aria-current="page">
+                                    <i className='bi bi-backspace-fill'></i>
+                                    <span className='ms-2 d-none d-sm-inline'>Log Out</span>
+                                </a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                    <div className="dropdown open">
+                        <a
+                            className="btn border-none dropdown-toggle text-white"
+                            type="button"
+                            id="triggerId"
+                            data-bs-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false"
+                        >
+                            <i className='bi bi-person f5-4'></i><span className='f5-4 ms-3'>Dropdown</span>
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="triggerId">
+                            <a className="dropdown-item" href="#">Profile</a>
+                            <a className="dropdown-item" href="#">Settings</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            {
-                menuItems.map((item, index) => (
-                    <NavLink to={item.path} key={index} className='link' activeclassName='active'>
-                        <div className="icon">{item.icon}</div>
-                        <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
-                    </NavLink>
-                ))
-            }
         </div>
-        <main>{Children}</main>
-    </div>
-  )
+    )
 }
+
 
 export default SideBar
